39a9a222a07b01d05f56c8bf54bfde92a48c9721
ALEX ALMEIDA <alexhavilla@gmail.com>
2022-05-14 14:39:43 -0300

    Save score

--- backend/src/main/java/com/devsuperior/dsmovies/entities/ScorePK.java
+++ backend/src/main/java/com/devsuperior/dsmovies/entities/ScorePK.java
@@ -38,7 +38,5 @@ public class ScorePK implements Serializable {
 	public void setUser(User user) {
 		this.user = user;
 	}
-	
-	
 
 }
--- backend/src/main/java/com/devsuperior/dsmovies/entities/User.java
+++ backend/src/main/java/com/devsuperior/dsmovies/entities/User.java
@@ -39,7 +39,5 @@ public class User {
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	
-	
 
 }
